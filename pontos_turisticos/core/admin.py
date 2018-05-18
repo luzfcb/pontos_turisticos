6fc6d8489afee236f32268b3b7b4e4629e0bdfe7
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2018-05-18 10:28:03 -0300

    implementado model PontoTuristico e modulo api

--- pontos_turisticos/core/admin.py
+++ pontos_turisticos/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.PontoTuristico)
+class PontoTuristicoAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome',
+        'descricao',
+        'aprovado'
+    )
