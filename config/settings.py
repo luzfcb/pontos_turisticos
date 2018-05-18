0dea7030969ad489ebb6dc98f9cd859dfcf3efc2
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2018-05-18 17:06:47 -0300

    adicionado app comentario

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'pontos_turisticos.core',
     'pontos_turisticos.atracoes',
+    'pontos_turisticos.comentarios',
+    'pontos_turisticos.core',
 ]
 
 MIDDLEWARE = [
